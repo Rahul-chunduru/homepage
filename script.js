aea3fd575a78b005ecae19dfd5bc148779890baa
pochr <32369340+Rahul-chunduru@users.noreply.github.com>
2020-08-03 14:24:34 +0530

    seperate pokepics for all.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var colours = [
-    { name: 'blue',   hex: '#2e7bcf' },
-    { name: 'orange', hex: '#f15f3e' },
-    { name: 'green',  hex: '#006400'},
-    { name: 'black',  hex: '#111111' }
+{ name: 'blue',   hex: '#2e7bcf' },
+{ name: 'orange', hex: '#f15f3e' },
+{ name: 'green',  hex: '#006400'},
+{ name: 'black',  hex: '#111111' }
 ];
 
 var colour_secondary = [
-    { name: 'blue',   hex: '#9ddcff' },
-    { name: 'orange', hex: '#fdbcb4' },
-     { name: 'green',  hex: '#ace1af'},
-    { name: 'black',  hex: '#696969' }
+{ name: 'blue',   hex: '#9ddcff' },
+{ name: 'orange', hex: '#fdbcb4' },
+{ name: 'green',  hex: '#ace1af'},
+{ name: 'black',  hex: '#696969' }
 ];
 
 // default
@@ -17,39 +17,39 @@ var colorIndex = 0;
 
 $(function()
 {
-    // add colours
-    var $palette = $("#palette");
-    for (var i = 0; i < colours.length; i++)
-    {
-        $palette.append($('<li class="colorItem" />').css({'background-color': colours[i].hex, 
-            'color': colour_secondary[i].hex}).data('index', i));
-    }
+// add colours
+var $palette = $("#palette");
+for (var i = 0; i < colours.length; i++)
+{
+    $palette.append($('<li class="colorItem" />').css({'background-color': colours[i].hex, 
+        'color': colour_secondary[i].hex}).data('index', i));
+}
 
-    var index = findGetParameter('colorIndex'); 
-    if(hasValue(index))
-    {
-        colorIndex = parseInt(index);
-    }
-    document.documentElement.style.setProperty("--primary-color",  colours[colorIndex].hex);
-    document.documentElement.style.setProperty("--secondary-color",  colour_secondary[colorIndex].hex); 
-    setBackground();
+var index = findGetParameter('colorIndex'); 
+if(hasValue(index))
+{
+    colorIndex = parseInt(index);
+}
+document.documentElement.style.setProperty("--primary-color",  colours[colorIndex].hex);
+document.documentElement.style.setProperty("--secondary-color",  colour_secondary[colorIndex].hex); 
+setBackground();
 
-    // add button functionality
-     $(document).on('click', '.colorItem', function()
-    {
-        // set colorIndex and be that color
-        colorIndex = $(this).data('index');
-        document.documentElement.style.setProperty("--primary-color",  $(this).css('background-color'));
-        document.documentElement.style.setProperty("--secondary-color",  $(this).css('color'));
-        setBackground();
-    });
+// add button functionality
+$(document).on('click', '.colorItem', function()
+{
+// set colorIndex and be that color
+colorIndex = $(this).data('index');
+document.documentElement.style.setProperty("--primary-color",  $(this).css('background-color'));
+document.documentElement.style.setProperty("--secondary-color",  $(this).css('color'));
+setBackground();
+});
 
-    // send that color through link
-    $(document).on('click', '.nav-bar-link', function()
-    {
-        var newLink = $(this).attr("href") + "?colorIndex=" + colorIndex; 
-        $(this).attr("href", newLink); 
-    })
+// send that color through link
+$(document).on('click', '.nav-bar-link', function()
+{
+    var newLink = $(this).attr("href") + "?colorIndex=" + colorIndex; 
+    $(this).attr("href", newLink); 
+})
 
 });
 
@@ -59,14 +59,14 @@ function hasValue(data) {
 
 function findGetParameter(parameterName) {
     var result = null,
-        tmp = [];
+    tmp = [];
     location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
     return result;
 }
 
@@ -74,14 +74,29 @@ function setBackground() {
 
     document.querySelector("#avatar").src = "assets/images/charmander.png";
     if(colorIndex == 0 || !hasValue(colorIndex))
-         {
-            document.querySelector('header').style.background = "url(assets/images/header-bg.jpg) 0 0 repeat-x"; 
-         }
-         else {
-            document.querySelector('header').style.background = "" ;
+    {
+        document.querySelector('header').style.background = "url(assets/images/header-bg.jpg) 0 0 repeat-x"; 
+    }
+    else {
+        document.querySelector('header').style.background = "" ;
+    }
 
-            if(colorIndex == 2) {
-                document.querySelector("#avatar").src = "assets/images/tropius.png";
-            }     
-         }
+    switch(colorIndex) {
+        case 0:
+        document.querySelector("#avatar").src = "assets/images/lapras.png";
+        break;
+        case 1:
+        document.querySelector("#avatar").src = "assets/images/charmander.png";
+        break;
+        case 2:
+        document.querySelector("#avatar").src = "assets/images/tropius.png";
+        break;
+        case 3:
+        document.querySelector("#avatar").src = "assets/images/absol.jpg";
+        break;
+        default:
+        break;
+    }
+
+
 }
